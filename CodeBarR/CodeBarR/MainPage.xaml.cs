c69d9daa5841c97cd4b5a7dedfd468f8b824804c
Rodrigo Alexandre Ceschi <rodrigoalexandre.sc@gmail.com>
2018-11-11 00:14:07 -0200

    inicio

---
+++ CodeBarR/CodeBarR/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile;
+using ZXing.Net.Mobile.Forms;
+
+namespace CodeBarR
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenScanner(object sender, EventArgs e)
+        {
+            Scanner();
+        }
+
+        public async void Scanner()
+        {
+            var scannerPage = new ZXingScannerPage(new ZXing.Mobile.MobileBarcodeScanningOptions
+            {                
+            });
+
+            scannerPage.OnScanResult += (result) =>
+            {
+                scannerPage.IsScanning = false;
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    Navigation.PopAsync();
+                    DisplayAlert("Código escaneado", result.Text, "OK");
+                });
+            };
+
+            await Navigation.PushAsync(scannerPage);
+        }
+    }
+}
